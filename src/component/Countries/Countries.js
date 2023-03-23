6dcc2ea3d6364368831cda283b8790382e5cf1c7
jahid morol <jahidmorol2@gmail.com>
2023-03-24 02:10:03 +0600

    almost done

--- src/component/Countries/Countries.js
+++ src/component/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -12,6 +13,7 @@ const Countries = () => {
   return (
     <div>
       <h1>Total Countries: {countries.length}</h1>
+      <div className="countries-container">
       {countries.map((country) => (
         <Country
           country={country}
@@ -22,6 +24,7 @@ const Countries = () => {
           area={country.area} */
         ></Country>
       ))}
+      </div>
     </div>
   );
 };
